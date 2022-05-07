1b9421c7d58e428ef7767b490ffaf4f3743d6422
Quyen Dinh <35825099+quyenjd@users.noreply.github.com>
2022-05-07 16:22:16 +1000

    Fix find command in playbook.sh

--- scripts/playbook.sh
+++ scripts/playbook.sh
@@ -3,6 +3,6 @@ git config --global --add safe.directory "/home/ubuntu/sd-ec2"
 cd /home/ubuntu/sd-ec2
 git reset --hard
 git pull
-find . -name "**/*.sh" -exec chmod a+x {} \;
+find . -wholename "./scripts/*.sh" -exec chmod a+x {} \;
 ./scripts/hash.sh "/home/ubuntu/sd-ec2" > "/var/www/portal_hash.txt"
 ansible-playbook playbook.yml
